b2aa7a7eea5ff9c2b9c859e23f091d63f67bcd96
wilderxD <wilder1093@gmail.com>
2026-08-17 16:26:24 -0500

    fix: historial completo

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://script.google.com/macros/s/AKfycbwPldYg2Dr3kgagokZaIbpDKlvMVbENgFDs4nwOA5gXkaThEWDD-2wTZ86kXCfOC5wq-g/exec',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbz_sEnjjun02MS0UOwchw2ieQ9GkclTJtzsRF7MMlIVVZ9EezJo9m9izS8mw_3DY0DOfQ/exec',
   apiToken: 'forli-9f2b7c41e8d3-2026-xtR7mK2q',
 };
